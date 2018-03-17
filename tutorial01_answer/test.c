50d0e5702da97eddf0fe76759bf2d131fc78bc7b
TopologyDing <937823519@qq.com>
2018-03-17 17:12:48 +0800

    1 finished

--- tutorial01_answer/test.c
+++ tutorial01_answer/test.c
@@ -83,5 +83,6 @@ static void test_parse() {
 int main() {
     test_parse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+	system("pause");
     return main_ret;
 }
